3db90e4110a9a99a8cd98337c6d1163207a58b27
Mehdi <khoshooei.m@gmail.com>
2025-02-26 12:03:47 +0100

    erste Änderung

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Laden der Daten
 df_math = pd.read_csv("student-mat.csv", sep=";")
@@ -12,20 +14,10 @@ df = pd.merge(df_math, df_por, on=merge_columns, suffixes=('_math', '_por'))
 grouped_by_gender = df.groupby("sex")[["G3_math", "G3_por"]].mean()
 print("Durchschnittliche Noten nach Geschlecht:\n", grouped_by_gender)
 
-import pandas as pd
-
 df = pd.read_csv("student-mat.csv", sep=";")
-
 # Berechnung von Mittelwert, Median und Standardabweichung der Noten
 stats = df[["G3"]].agg(["mean", "median", "std"])
 print("Deskriptive Statistik der Noten:\n", stats)
-
-
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("student-mat.csv", sep=";")
 
 # Scatterplot zur Untersuchung der Korrelation zwischen Abwesenheiten und Noten
 sns.scatterplot(x=df["absences"], y=df["G3"])
