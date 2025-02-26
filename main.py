c31d0f9ec235dd55abe802a9290304404110da0a
Mehdi <khoshooei.m@gmail.com>
2025-02-26 10:40:36 +0100

    Durchschnittliche Noten nach Geschlecht

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Laden der Daten
+df_math = pd.read_csv("student-mat.csv", sep=";")
+df_por = pd.read_csv("student-por.csv", sep=";")
+
+# Zusammenführen der beiden Datensätze
+merge_columns = ["school", "sex", "age", "address", "famsize", "Pstatus", "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery", "internet"]
+df = pd.merge(df_math, df_por, on=merge_columns, suffixes=('_math', '_por'))
+
+# Gruppierung nach Geschlecht
+grouped_by_gender = df.groupby("sex")[["G3_math", "G3_por"]].mean()
+print("Durchschnittliche Noten nach Geschlecht:\n", grouped_by_gender)
