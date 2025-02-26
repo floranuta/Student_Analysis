60ffe79f5ce14486f43dc980ddb352b67107e4d8
vladimir707 <vovanew707@gmail.com>
2025-02-26 21:45:56 +0100

    pdf bericht

--- StudentPerformanceAnalysis.py
+++ StudentPerformanceAnalysis.py
@@ -6,10 +6,13 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 import math
+from fpdf import FPDF
+from datetime import datetime
+import os
 
 root = Tk()
 root.title("Student Performance Analysis")
-root.geometry("600x250")
+root.geometry("600x300")
 
 root.grid_rowconfigure(index=0, weight=1)
 root.grid_columnconfigure(index=0, weight=1)
@@ -18,10 +21,10 @@ root.grid_columnconfigure(index=1, weight=1)
 filepath = ""  # path
 df_mat = None  # DataFrame 
 spalt = []
-
+pdf = FPDF()
 # Datei öfnen
 def open_file():
-    global filepath, df_mat, spalt
+    global filepath, df_mat, spalt, filepath
     filepath = filedialog.askopenfilename(title="Choose a file", filetypes=(("CSV files", "*.csv"), ("All files", "*.*")))
     if filepath:  
         print(f"File selected: {filepath}")
@@ -37,9 +40,14 @@ def open_file():
         calculate_button["state"] = "normal"
         calculate_button1["state"] = "normal"
         gruppen['values'] = spalt
-        
+selected_spalte=""   
+kor = 0    
+filename = "" 
+filename2 = "" 
+ipdf = 0
 # statistic calculation
 def calculate_stats():
+    global selected_spalte, kor, filename, ipdf, pdf, filepath #variable für PDF
     if df_mat is None:
         print("wählen zuerst ein Datei aus")
         return
@@ -49,8 +57,20 @@ def calculate_stats():
     if selected_colum not in ["first period", "second period", "final"]:
         print("wählen  eine bestimte Spalte aus")
         return
+    if enabled1.get() == 1:
+        selected_spalte = selected_colum
+        add_to_pdf["state"] = "normal"
+        pdf = FPDF()
+        pdf.set_auto_page_break(auto=True, margin=15)
+        pdf.add_page()
+        pdf.set_font("Arial", style='', size=12)
 
-    print(f"Spalte: {selected_colum}")
+        # Title
+        pdf.set_font("Arial", style='B', size=16)
+        pdf.cell(200, 10, f"Bericht zur Datenanalyse, Datei {os.path.basename(filepath)}", ln=True, align='C')
+        ipdf=0
+    else:
+        print(f"Spalte: {selected_colum}")
     if selected_colum == "first period":
         selected_column = "G1"
     elif selected_colum == "second period":
@@ -59,7 +79,10 @@ def calculate_stats():
         selected_column = "G3"
     # Korrelationskoeffizient
     koef_cor = df_mat[selected_column].corr(df_mat["anwesenheit"])
-    print(f"Korrelationskoeffizient ({selected_column} и Anwesenheit): {koef_cor:.4f}")
+    if enabled1.get()==1:
+        kor = koef_cor
+    else:
+        print(f"Korrelationskoeffizient ({selected_column} und Anwesenheit): {koef_cor:.4f}")
 
     # H-statistik
     anova_results = {}
@@ -93,12 +116,22 @@ def calculate_stats():
     plt.title(f'H-statistic: exp(-p) ({selected_column})')
     plt.legend()
     plt.grid(axis='y', linestyle='--', alpha=0.6)
+    # bild für H-statistik zeigen oder speichern
+    if enabled1.get() == 1:
+        filename = f"Figure_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
+        plt.savefig(filename)
+        plt.close()
+    else:
+        
+        plt.show()
 
-    plt.show()
+
+  
 
 
-
+selected_gruppe = ""
 def calculate_grup():
+    global selected_spalte, selected_gruppe, filename2, ipdf, pdf #variable für PDF
     if df_mat is None:
         print("wählen zuerst ein Datei aus")
         return
@@ -114,7 +147,22 @@ def calculate_grup():
         return
     
 
-    print(f"Spalte: {selected_colum}")
+    if enabled2.get() == 1:
+        selected_spalte = selected_colum
+        selected_gruppe = gruppens
+        add_to_pdf["state"] = "normal"
+        pdf = FPDF()
+        pdf.set_auto_page_break(auto=True, margin=15)
+        pdf.add_page()
+        pdf.set_font("Arial", style='', size=12)
+
+        # Title
+        pdf.set_font("Arial", style='B', size=14)
+        pdf.cell(200, 10, f"Bericht zur Datenanalyse, Datei {os.path.basename(filepath)}", ln=True, align='C')
+        ipdf=0
+    else:
+        print(f"Spalte: {selected_colum}")
+        print(f"Gruppe: {selected_gruppe}")
     if selected_colum == "first period":
         selected_column = "G1"
     elif selected_colum == "second period":
@@ -156,11 +204,53 @@ def calculate_grup():
         ax.set_xlabel("Groupen")
         ax.set_ylim(0, max(mean_values.max(), median_values.max(), stabw_values.max()) * 1.2)
         ax.grid(axis='y', linestyle='--', alpha=0.6)
+    
+    # bild für Gruppenstatistik  zeigen oder speichern
+    if enabled2.get() == 1:
+        #plt.tight_layout()
+        filename2 = f"Figure_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
+        plt.savefig(filename2)
+        plt.close()
+    else:
+        #plt.tight_layout()
+        plt.show()
 
-    plt.tight_layout()
-    plt.show()
+# add to PDF
+def addtopdf():
+    global selected_spalte, selected_gruppe, filename2, filename, kor, pdf
+    if enabled1.get() == 1:
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0, 10, f"analysierte Spalte: {selected_spalte}")
+        pdf.ln(5)
+        pdf.multi_cell(0, 10, f"Korrelationskoeffizient zwischen akademischen Leistungen und Anwesenheit für {selected_spalte}: {kor:.2f}")
+        pdf.ln(5)
+        pdf.multi_cell(0, 10, f"Parameter, die den größten Einfluss auf die Leistung hatten")
+        pdf.ln(10)
+        pdf.image(filename, x=10, w=180)
+        speich_pdf["state"] = "normal"
+    if enabled2.get() == 1:
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0, 10, f"analysierte Spalte: {selected_spalte}")
+        pdf.ln(5)
+        
+        pdf.multi_cell(0, 10, f"Mittelwert, Median und Standardabweichung der Noten für {selected_gruppe}")
+        pdf.ln(10)
+        pdf.image(filename2, x=10, w=180)
+        speich_pdf["state"] = "normal"
+    selected_spalte=""   
+    selected_gruppe = ""
+    kor = 0    
+    filename = "" 
+    filename2 = "" 
+    enabled1.set(0) #flag delite
+    enabled2.set(0)
+    add_to_pdf["state"]="disabled"
 
+    # Сspeichern PDF
 
+def speichpdf():
+    pdf.output(f"report_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.pdf")
+    
 # öffnen taste
 open_button = ttk.Button(root, text="Datei öfnen", command=open_file)
 open_button.grid(column=0, row=1, sticky=NSEW, padx=10, pady=5)
@@ -174,6 +264,10 @@ column_selector.set("wählen eine Spalte aus")
 calculate_button = ttk.Button(root, text="Kalkulieren", command=calculate_stats, state="disabled")
 calculate_button.grid(column=0, row=3, sticky=NSEW, padx=10, pady=5)
 
+#Checkbutton für grafik speichern
+enabled1 = IntVar()
+bild_speichern = ttk.Checkbutton(text="Bild speichern", variable=enabled1)
+bild_speichern.grid(column=1, row=3, sticky=NSEW, padx=10, pady=5)
 #  Checkbutton für nur kriterienswerte
 enabled = IntVar()
 kriteriumswerte1 = ttk.Checkbutton(text="Nur Kriteriumswerte", variable=enabled)
@@ -183,5 +277,24 @@ gruppen.grid(column=0, row=5, sticky=NSEW, padx=10, pady=5)
 gruppen.set("wählen eine Grupe aus")
 calculate_button1 = ttk.Button(root, text="Kalkulieren", command=calculate_grup, state="disabled")
 calculate_button1.grid(column=0, row=6, sticky=NSEW, padx=10, pady=5)
+#Checkbutton für grafik speichern
+enabled2 = IntVar()
+bild_speichern2 = ttk.Checkbutton(text="Bild speichern", variable=enabled2)
+bild_speichern2.grid(column=1, row=6, sticky=NSEW, padx=10, pady=5)
+add_to_pdf = ttk.Button(root, text="Add to PDF", command=addtopdf, state="disabled")
+add_to_pdf.grid(column=0, row=7, sticky=NSEW, padx=10, pady=5)
+speich_pdf = ttk.Button(root, text="Speichern PDF", command=speichpdf, state="disabled")
+speich_pdf.grid(column=0, row=8, sticky=NSEW, padx=10, pady=5)
+
+
+
 
 root.mainloop()
+
+
+
+
+
+
+
+
